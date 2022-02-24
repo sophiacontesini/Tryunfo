c101d4d2092140bbaefd9f7abd602ba2b0f009d5
sophiacontesini <sophiamarques214@gmail.com>
2022-02-24 16:02:02 -0300

    requisito 4 ok

--- src/App.js
+++ src/App.js
@@ -3,12 +3,54 @@ import Card from './components/Card';
 import Form from './components/Form';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      description: '',
+      number1: '',
+      number2: '',
+      number3: '',
+      img: '',
+      rare: '',
+      check: false,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value, id } = target;
+    this.setState({
+      [name]: id === 'check' ? target.checked : value,
+    });
+  }
+
   render() {
+    const { name, description, number1, number2, number3, img, rare, check } = this.state;
     return (
       <div>
         <h1>Tryunfo</h1>
-        <Form />
-        <Card />
+        <Form
+          onInputChange={ this.handleChange }
+          cardName={ name }
+          cardDescription={ description }
+          cardAttr1={ number1 }
+          cardAttr2={ number2 }
+          cardAttr3={ number3 }
+          cardImage={ img }
+          cardRare={ rare }
+          cardTrunfo={ check }
+        />
+        <Card
+          onInputChange={ this.handleChange }
+          cardName={ name }
+          cardDescription={ description }
+          cardAttr1={ number1 }
+          cardAttr2={ number2 }
+          cardAttr3={ number3 }
+          cardImage={ img }
+          cardRare={ rare }
+          cardTrunfo={ check }
+        />
       </div>
     );
   }
